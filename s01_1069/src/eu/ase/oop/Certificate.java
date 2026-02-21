6e9674f51a0877a03c3c5ba3380c3a0aca0aabca
daria-cirjau <dariaaax@yahoo.com>
2026-02-21 12:08:18 +0200

    add s01

---
+++ s01_1069/src/eu/ase/oop/Certificate.java
@@ -0,0 +1,39 @@
+package eu.ase.oop;
+
+public class Certificate extends Object {
+    private int id;
+    private String name;
+
+    public Certificate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Certificate myClone() {
+        Certificate newCert = new Certificate(this.id, this.name);
+        return newCert;
+    }
+
+    public boolean equals(Certificate cert) {
+        if(this.id == cert.getId() && this.name.equals(cert.getName())) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ s01_1069/src/eu/ase/oop/ProgMainCertificate.java
@@ -0,0 +1,36 @@
+package eu.ase.oop;
+
+public class ProgMainCertificate {
+    public static void main(String[] args) {
+        Certificate c1 = new Certificate(777, "C1");
+        Certificate c2 = new Certificate(999, "C2");
+
+        int x = 10;
+
+       // System.out.println("c1: " + c1.getId() + " " + c1.getName());
+        // System.out.println("c2: " + c2.getId() + " " + c2.getName());
+
+        c2 = c1;
+        c2.setId(333);
+
+        System.out.println("c1: " + c1.getId() + " " + c1.getName());
+        System.out.println("c2: " + c2.getId() + " " + c2.getName());
+
+        Certificate c3 = c1.myClone();
+        System.out.println("c3: " + c3.getId() + " " + c3.getName());
+        System.out.println(c1 + " " + c2 + " " + c3);
+        c3.setId(444);
+
+        System.out.println("c1: " + c1.getId() + " " + c1.getName());
+        System.out.println("c3: " + c3.getId() + " " + c3.getName());
+
+        boolean eq = c1 == c2;
+        System.out.println("c1 == c2 " + eq);
+        eq = c1 == c3;
+        System.out.println("c1 == c3 " + eq);
+
+        boolean equals = c1.equals(c3);
+        System.out.println("c1 equals c3 " + equals);
+    }
+
+}
